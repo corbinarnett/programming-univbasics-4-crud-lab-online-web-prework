fdd55cfc04306d5de3ca490e52f56770b1c5bf41
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-12 06:09:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array.unshift(element)
+add_element_to_end_of_array.pop(element)
 end
 
 def add_element_to_start_of_array(array, element)
